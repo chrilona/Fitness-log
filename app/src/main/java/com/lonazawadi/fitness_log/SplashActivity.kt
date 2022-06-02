4cee955a0f106d5e67696f318f2741df58140091
chrilona <christabelalona@gmail>
2022-06-02 17:32:31 +0300

    added splash screen

--- app/src/main/java/com/lonazawadi/fitness_log/SplashActivity.kt
+++ app/src/main/java/com/lonazawadi/fitness_log/SplashActivity.kt
@@ -5,6 +5,6 @@ import android.os.Bundle
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        
     }
 }
